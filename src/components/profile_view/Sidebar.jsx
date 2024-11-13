214f94ab55d49aac4ab475cbbf4009d6a27f5944
Vigan Ejupi <vejupi1@charlotte.edu>
2024-11-13 01:33:57 -0500

    added logos and images to sidebar

--- src/components/profile_view/Sidebar.jsx
+++ src/components/profile_view/Sidebar.jsx
@@ -1,41 +1,49 @@
 import '../../index.css';
+import bitmoji from '../../assets/bitmoji.png'; // Import the image
+import msgIcon from '../../assets/msg.webp'; // Import the msg icon image
+import addIcon from '../../assets/add.png'; // Import the add icon image
 
 function Sidebar() {
     return (
         <div className="sidebar">
-        <img src="../../assets/bitmoji.png"></img>
-        <div style={{height: '40%'}}></div>
-        <h2>Quandale Dingle</h2>
-        <h3>@quandale_dingle100</h3>
-        <div class="msg-add-icons">
-            <img src="images/msg.webp"></img>
-            <img src="images/add.png"></img>
-        </div>
-        <ul aria-label="Quandale's Friends" class="profile-friends">
-            <li>
-                <img src="images/bitmoji.png"></img>
-                <p>@speed</p>
-            </li>
-            <li>
-                <img src="images/bitmoji.png"></img>
-                <p>@speed</p>
-            </li>
-            <li>
-                <img src="images/bitmoji.png"></img>
-                <p>@speed</p>
-            </li>
-            <li>
-                <img src="images/bitmoji.png"></img>
-                <p>@speed</p>
-            </li>
-            <li>
-                <img src="images/bitmoji.png"></img>
-                <p>@speed</p>
-            </li>
+            {/* Profile Image */}
+            <img src={bitmoji} alt="User Bitmoji" style={{ width: '100px', height: '100px', borderRadius: '50%' }} />
             
-        </ul>
-    </div>
+            <div style={{height: '40%'}}></div>
+            <h2>Quandale Dingle</h2>
+            <h3>@quandale_dingle100</h3>
+            
+            {/* Message and Add Icons */}
+            <div className="msg-add-icons">
+                <img src={msgIcon} alt="Message Icon" style={{ width: '30px', height: '30px' }} />
+                <img src={addIcon} alt="Add Icon" style={{ width: '30px', height: '30px' }} />
+            </div>
+
+            {/* Friends List */}
+            <ul aria-label="Quandale's Friends" className="profile-friends">
+                <li>
+                    <img src={bitmoji} alt="Friend Bitmoji" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                    <p>@speed</p>
+                </li>
+                <li>
+                    <img src={bitmoji} alt="Friend Bitmoji" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                    <p>@speed</p>
+                </li>
+                <li>
+                    <img src={bitmoji} alt="Friend Bitmoji" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                    <p>@speed</p>
+                </li>
+                <li>
+                    <img src={bitmoji} alt="Friend Bitmoji" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                    <p>@speed</p>
+                </li>
+                <li>
+                    <img src={bitmoji} alt="Friend Bitmoji" style={{ width: '30px', height: '30px', borderRadius: '50%' }} />
+                    <p>@speed</p>
+                </li>
+            </ul>
+        </div>
     );
-};
+}
 
 export default Sidebar;
