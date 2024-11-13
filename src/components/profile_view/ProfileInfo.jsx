4dc1669f6f70eef050f06acb3879ec48ca81a82a
Vigan Ejupi <vejupi1@charlotte.edu>
2024-11-13 01:32:16 -0500

    Add user Bitmoji image to ProfileInfo component

--- src/components/profile_view/ProfileInfo.jsx
+++ src/components/profile_view/ProfileInfo.jsx
@@ -1,11 +1,34 @@
 import '../../index.css';
+import bitmoji from '../../assets/bitmoji.png'; 
 
 function ProfileInfo() {
     return (
-        <div className="profile-info"> <h1 style={{background: "rgb(223,220,240)", 
-            background: "linear-gradient(90deg, rgba(223,220,240,1) 0%, rgba(118,102,162,1) 100%)", 
-            border: "1px solid black", padding: "10%", width: "180%", textAlign: "center", paddingTop: "4%", paddingBottom: "4%"}}>Profile Information</h1> </div>
+        <div className="profile-info">
+            <h1 style={{
+                background: "linear-gradient(90deg, rgba(223,220,240,1) 0%, rgba(118,102,162,1) 100%)", 
+                border: "1px solid black", 
+                padding: "10%", 
+                width: "180%", 
+                textAlign: "center", 
+                paddingTop: "4%", 
+                paddingBottom: "4%"
+            }}>
+                Profile Information
+            </h1>
+            
+            {/* Image Element */}
+            <img 
+                src={bitmoji} 
+                alt="User Bitmoji" 
+                style={{ 
+                    width: '150px', 
+                    height: '150px', 
+                    borderRadius: '50%', 
+                    marginTop: '20px' 
+                }} 
+            />
+        </div>
     );
-};
+}
 
 export default ProfileInfo;
